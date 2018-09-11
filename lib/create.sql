037bf56f36b62eeb74f13f5671df91a84ea18305
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-11 21:17:42 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projects (
+        id INTEGER PRIMARY KEY,
+                title TEXT, 
+                funding_goal INTEGER,
+                start_date REAL,
+                end_date REAL,
+            );
