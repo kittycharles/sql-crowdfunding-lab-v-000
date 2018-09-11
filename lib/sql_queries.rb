cda9c44e5241c95d9348cd943f1dc7944f3d2e7a
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-11 21:42:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,9 +13,9 @@ GROUP BY title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, sum(pledges.amount) FROM users
-JOIN pledges on users.id = pledges.project_id
-GROUP BY users.name"
+  "SELECT users.name, users.age, sum(pledges.amount) FROM users
+  JOIN pledges ON users.id = pledges.user_id
+  GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
