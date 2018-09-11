e51dfe419daeaf45b028eff22dbd4f015d50139e
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-11 21:29:31 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,42 @@
+-- CREATE TABLE projects (
+--         id INTEGER PRIMARY KEY,
+--                 title TEXT,
+--                 category TEXT,
+--                 funding_goal INTEGER,
+--                 start_date TEXT,
+--                 end_date TEXT
+--             );
+--
+-- CREATE TABLE user (
+--         id INTEGER PRIMARY KEY,
+--                 name TEXT,
+--                 age INTEGER
+--               );
+--
+-- CREATE TABLE pledge (
+--       id INTEGER PRIMARY KEY
+--                 amount INTEGER,
+--                 user_id INTEGER,
+--                 project_id INTEGER
+--               );
 CREATE TABLE projects (
-        id INTEGER PRIMARY KEY,
-                title TEXT,
-                category TEXT,
-                funding_goal INTEGER,
-                start_date TEXT,
-                end_date TEXT
-            );
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
 
-CREATE TABLE user (
-        id INTEGER PRIMARY KEY,
-                name TEXT,
-                age INTEGER
-              );
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
-CREATE TABLE pledge (
-      id INTEGER PRIMARY KEY
-                amount INTEGER,
-                user_id INTEGER,
-                project_id INTEGER
-              );
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  project_ID INTEGER,
+  user_ID INTEGER
+);
