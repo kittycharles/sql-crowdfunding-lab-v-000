380af5701bd87331da2f613eadcb978c2dcd76eb
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-11 21:20:50 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,20 @@
 CREATE TABLE projects (
         id INTEGER PRIMARY KEY,
-                title TEXT, 
+                title TEXT,
                 funding_goal INTEGER,
                 start_date REAL,
                 end_date REAL,
             );
+
+CREATE TABLE user (
+        id INTEGER PRIMARY KEY,
+                name TEXT,
+                age INTEGER,
+              );
+
+CREATE TABLE pledge (
+      id INTEGER PRIMARY KEY
+                amount INTEGER,
+                user_id INTEGER,
+                project_id, INTEGER
+              );
